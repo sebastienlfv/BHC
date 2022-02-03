6fd5395a72e14aea57414d55aa75d5fdd5fd8d99
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-02-03 14:42:35 +0100

    *

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-new fullpage('main', {
-    navigation: true,
-    responsiveWidth: 700,
-    anchors: ['#page1', '#page2', '#page3'],
-    parallax: true,
-    onLeave: function(origin, destination, direction){
-        console.log("Leaving section" + origin.index);
-    },
-});
