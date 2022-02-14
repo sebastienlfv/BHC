be15dc39d6f5d7852e9d8f5b78d1ebb83968f519
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-02-14 18:11:40 +0100

    html page1 ok

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let streampack = document.getElementsByClassName('streampack-onglet');
+let streampackOnglet = document.getElementsByClassName('page-2-services__container__streampack');
+
+function streampackDisplay(){
+    streampackOnglet.setAttribute('style','display: flex');
+};
