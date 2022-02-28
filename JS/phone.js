9d2e342677e42ce3b7085ff368a794b7ab669042
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-02-28 16:19:19 +0100

    *

--- JS/phone.js
+++ JS/phone.js
@@ -1,1 +1,18 @@
 
+function openBurger() {
+    document.getElementById('burger-content').style.display = "flex";
+
+    if (document.getElementById('burger-content').style.display = "flex"){
+        document.getElementById('btn-burger').style.display = "none";
+        document.getElementById('close-burger').style.display = "flex";
+    }
+}
+
+function closeBurger() {
+    document.getElementById('burger-content').style.display = "none";
+
+    if (document.getElementById('burger-content').style.display = "none"){
+        document.getElementById('btn-burger').style.display = "flex";
+        document.getElementById('close-burger').style.display = "none";
+    }
+}
