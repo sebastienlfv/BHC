e45ae649729b99d2e78bdbf660e5c4f9c3c6cbaa
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-02-28 17:36:14 +0100

    é*é

--- JS/test.js
+++ JS/test.js
@@ -1,13 +1,17 @@
-function changeAvis() {
+function changeAvisBottom() {
     if (document.getElementById('avis1').style.display = "flex"){
-        document.getElementById('avis1').style.display = "none";
-        document.getElementById('avis2').style.display = "none";
-        document.getElementById('avis3').style.display = "flex";
-        document.getElementById('avis4').style.display = "flex";
-    } else if(document.getElementById('avis3').style.display = "flex"){
-        document.getElementById('avis1').style.display = "flex";
-        document.getElementById('avis2').style.display = "flex";
-        document.getElementById('avis3').style.display = "none";
-        document.getElementById('avis4').style.display = "none";
+        document.getElementById('avis1').style.display = "none"
+        document.getElementById('avis2').style.display = "none"
+        document.getElementById('avis3').style.display = "flex"
+        document.getElementById('avis4').style.display = "flex"
+    }
+}
+
+function changeAvisTop() {
+    if (document.getElementById('avis3').style.display = "flex"){
+        document.getElementById('avis1').style.display = "flex"
+        document.getElementById('avis2').style.display = "flex"
+        document.getElementById('avis3').style.display = "none"
+        document.getElementById('avis4').style.display = "none"
     }
 }
